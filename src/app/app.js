89ee4a4dc1b340b224f908d1849ddd790ac9cd76
houssam-dev <developpeur.pro.ma@gmail.com>
2022-06-15 11:05:48 +0100

    fix main get for api

--- src/app/app.js
+++ src/app/app.js
@@ -79,7 +79,7 @@ function startup() {
 	// Middleware
 	app.use(cors());
 
-	app.get('/', (req, res) => res.render('index'));
+	app.get('/', (req, res) => res.json({ msg: 'welcome to uploader-image' }));
 
 	app.post('/upload', (req, res) => {
 		console.log(`📦 +++ new request `);
